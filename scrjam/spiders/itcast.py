fd664cdcead1b49e2572ed28bb8e764922ae9a5a
xieyuanzheng <121546683@qq.com>
2019-04-16 00:51:23 +0800

    创建分支1

--- scrjam/spiders/itcast.py
+++ scrjam/spiders/itcast.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrjam.items import ScrjamItem
 
 
 class ItcastSpider(scrapy.Spider):
     name = 'itcast'
     allowed_domains = ['itcast.cn']
-    start_urls = ['http://itcast.cn/']
+    start_urls = ['http://www.itcast.cn/']
 
     def parse(self, response):
-        print("这是一个爬虫的开始")
-        pass
+        print("这是一个爬虫的开始,for branch1")
+        item = ScrjamItem()
+        item['title'] = response.xpath('/html/head/title/text()').extract()
+        item['link'] = response.xpath('/html/body/div[1]/div[2]/div[1]/div[2]/ul/li[2]/a/@href').extract()
+        item['desc'] = response.xpath('/html/body/div[1]/div[12]/div/div[1]/p/text()').extract()
+        print(item['link'])
+        print("这是一个爬虫的结束")
+        yield item
